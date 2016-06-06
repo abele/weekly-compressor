88c5ca1d3ee6cdaace3854ed64b55dd47a1e1671
Jānis Ābele <janis.abele@gmail.com>
2016-06-06 18:06:38 +0300

    Add module so it's posible to import funtions into other modules

--- wc.py
+++ wc.py
@@ -1,8 +1,8 @@
 # -*- encoding: utf-8 -*-
 import os
-import pdb
 import shelve
 import webbrowser
+import sys
 
 import imapy
 from imapy.query_builder import Q
@@ -12,53 +12,72 @@ HOST = os.environ['WC_HOST']
 USERNAME = os.environ['WC_USERNAME']
 PASSWORD = os.environ['WC_PASSWORD']
 
+def main():
+    box = imapy.connect(
+        host=HOST,
+        username=USERNAME,
+        password=PASSWORD,
+        ssl=True,
+    )
 
-box = imapy.connect(
-    host=HOST,
-    username=USERNAME,
-    password=PASSWORD,
-    ssl=True,
-)
+    q = Q()
 
-q = Q()
+    emails = box.folder('INBOX').emails(
+        q.sender("pycoders.com")
+    )
+    DONE_FOLDER = 'weekly' + box.separator + '1'
 
-emails = box.folder('INBOX').emails(
-    q.sender("pycoders.com")
-)
-DONE_FOLDER = 'weekly' + box.separator + '1'
+    if DONE_FOLDER not in box.folders():
+        base, sub = DONE_FOLDER.split(box.separator)
+        box.make_folder(base)
+        box.folder(base).make_folder(sub)
 
-if DONE_FOLDER not in box.folders():
-    base, sub = DONE_FOLDER.split(box.separator)
-    box.make_folder(base)
-    box.folder(base).make_folder(sub)
+    print(box.folders())
 
-print(box.folders())
-
-with shelve.open('wc.db', writeback=True) as db:
-    if 'emails' not in db:
-        db['emails'] = {}
-    if 'links' not in db:
-        db['links'] = set()
+    with shelve.open('wc.db', writeback=True) as db:
+        if 'emails' not in db:
+            db['emails'] = {}
+        if 'links' not in db:
+            db['links'] = set()
 
 
-with shelve.open('wc.db', writeback=True) as db:
+    with shelve.open('wc.db', writeback=True) as db:
+        for email in emails:
+            db['emails'][email['subject']] = {
+                'headers': email['headers'],
+                'from': email['from'],
+                'from_email': email['from_email'],
+                'from_whom': email['from_whom'],
+                'to': email['to'],
+                'flags': email['flags'],
+                'subject': email['subject'],
+                'cc': email['cc'],
+                'date': email['date'],
+                'text_normalized': email['text'][0]['text_normalized'],
+            }
+
+
     for email in emails:
-        db['emails'][email['subject']] = {
-            'headers': email['headers'],
-            'from': email['from'],
-            'from_email': email['from_email'],
-            'from_whom': email['from_whom'],
-            'to': email['to'],
-            'flags': email['flags'],
-            'subject': email['subject'],
-            'cc': email['cc'],
-            'date': email['date'],
-            'text_normalized': email['text'][0]['text_normalized'],
-        }
+        email.move(DONE_FOLDER)
+
+    with shelve.open('wc.db', writeback=True) as db:
+        for subject, email in db['emails'].items():
+            links = set(parse_links(email['text_normalized']))
+            for email in emails:
+                db['links'].update(set(links))
 
 
-for email in emails:
-    email.move(DONE_FOLDER)
+
+    with shelve.open('wc.db') as db:
+        for chunk in chunked(db['links'], 7):  # XXX: 7 is magic ;)
+            for link in chunk:
+                if (not is_tw_action(link) and not is_instapaper(link) 
+                    and not is_unsubscribe(link)):
+                    webbrowser.open_new_tab(link)
+
+            con = input('Next batch (Y/n)')
+            if con.lower().startswith('n'):
+                break
 
 def parse_links(text):
     for line in text.split('('):
@@ -76,21 +95,5 @@ def is_tw_action(link):
     return link.startswith('http://twitter.com/intent/tweet')
 
 
-with shelve.open('wc.db', writeback=True) as db:
-    for subject, email in db['emails'].items():
-        links = set(parse_links(email['text_normalized']))
-        for email in emails:
-            db['links'].update(set(links))
-
-
-
-with shelve.open('wc.db') as db:
-    for chunk in chunked(db['links'], 7):  # XXX: 7 is magic ;)
-        for link in chunk:
-            if (not is_tw_action(link) and not is_instapaper(link) 
-                and not is_unsubscribe(link)):
-                webbrowser.open_new_tab(link)
-
-        con = input('Next batch (Y/n)')
-        if con.lower().startswith('n'):
-            break
+if __name__ == '__main__':
+    sys.exit(main())
